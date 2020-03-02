17dd66bd9b7b2598c84bee5a2c3de306a65715ac
jag.dhillon <jag.dhillon@nhvr.gov.au>
2020-03-02 14:31:17 +1000

    Assignment complete... special matrix inverse cache works.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implementation of a special matrix which caches its inverse.
+## This is more efficient as matrix inverse calculation is an 
+## expensive operation
 
-## Write a short comment describing this function
 
+## makeCacheMatrix creates a special matrix from an ordinary matrix
 makeCacheMatrix <- function(x = matrix()) {
+	## invx stores the inverse of the matrix
+	invx <- NULL
 
+	##Initiallise the matrix and its mean
+	set <- function( amatrix) {
+		x <<- amatrix
+		invx <<- NULL
+	}
+	##Get the matrix
+	get <- function() x
+
+	##Set the matrix inverse
+	setinv <- function( inv) invx <<- inv
+	##Get the matrix inverse
+	getinv <- function() invx
+	list( set=set, get=get,
+		setinv = setinv,
+		getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve takes in a special matrix and returns it cached inverse,
+## or calculates its inverse and caches it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+	invmat <- x$getinv()
+	if ( !is.null(invmat) ) {
+		message("getting cached inverse")
+		return( invmat)
+	}
+	mat <- x$get()
+	invmat <- solve( mat, ...) ##Assumption: Inverse of mat exists
+	x$setinv( invmat)
+	invmat	
 }
